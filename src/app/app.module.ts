b76d8a17d4584ce50c7a491564301f154212f85b
rakesh1 <jafange123@gmail.com>
2021-06-13 10:17:22 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
+// import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { TextfieldSmallComponent } from './textfield-small/textfield-small.component';
 import { TextFieldLargeComponent } from './text-field-large/text-field-large.component';
@@ -8,6 +8,7 @@ import { ButtonPrimaryComponent } from './button-primary/button-primary.componen
 import { ButtonSecondaryComponent } from './button-secondary/button-secondary.component';
 import { ButtonTertiaryComponent } from './button-tertiary/button-tertiary.component';
 import { ToggleComponent } from './toggle/toggle.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 
 @NgModule({
@@ -19,6 +20,8 @@ import { ToggleComponent } from './toggle/toggle.component';
     ButtonSecondaryComponent,
     ButtonTertiaryComponent,
     ToggleComponent,
+    CheckboxComponent,
+    // MatIconModule
   ],
   imports: [
     BrowserModule
